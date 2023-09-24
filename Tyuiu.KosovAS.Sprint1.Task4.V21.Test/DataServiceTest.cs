f534ac9778c6b52f279fcbe5419d75027cddcd15
Tyuiu.KosovAS@outlook.com <andrey.kosov.05@inbox.ru>
2023-09-24 11:42:35 +0500

    Task4

---
+++ Tyuiu.KosovAS.Sprint1.Task4.V21.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KosovAS.Sprint1.Task4.V21.Lib;
+
+namespace Tyuiu.KosovAS.Sprint1.Task4.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 3;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(1.2, res);
+        }
+    }
+}
--- Tyuiu.KosovAS.Sprint1.Task4.V21/Program.cs
+++ Tyuiu.KosovAS.Sprint1.Task4.V21/Program.cs
@@ -4,53 +4,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using Tyuiu.KosovAS.Sprint1.Task3.V2.Lib;
+using Tyuiu.KosovAS.Sprint1.Task4.V21.Lib;
 
-namespace Tyuiu.KosovAS.Sprint1.Task3.V2
+namespace Tyuiu.KosovAS.Sprint1.Task4.V21
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             DataService ds = new DataService();
-            double priceNote; int amountNote; double pricePencil; int amountPencil;
+            double x; double y;
+
 
             Console.Title = "Спринт #1 | Выполнил: Косов А. С. | АСОиУБ-23-3";
             Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
             Console.WriteLine("* Спринт #1" + String.Concat(Enumerable.Repeat(" ", 63)) + "*");
-            Console.WriteLine("* Тема: Операторы составного присваивания" + String.Concat(Enumerable.Repeat(" ", 33)) + "*");
-            Console.WriteLine("* Задание #3" + String.Concat(Enumerable.Repeat(" ", 62)) + "*");
-            Console.WriteLine("* Вариант #2" + String.Concat(Enumerable.Repeat(" ", 62)) + "*");
+            Console.WriteLine("* Тема: Class Math" + String.Concat(Enumerable.Repeat(" ", 56)) + "*");
+            Console.WriteLine("* Задание #4" + String.Concat(Enumerable.Repeat(" ", 62)) + "*");
+            Console.WriteLine("* Вариант #21" + String.Concat(Enumerable.Repeat(" ", 61)) + "*");
             Console.WriteLine("* Выполнил: Косов Андрей Сергеевич | АСОиУБ-23-3" + String.Concat(Enumerable.Repeat(" ", 26)) + "*");
             Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
             Console.WriteLine("* УСЛОВИЕ:" + String.Concat(Enumerable.Repeat(" ", 64)) + "*");
             Console.WriteLine("* Написать програму, которая запрашивает у пользователя исходные данные,  *");
-            Console.WriteLine("* вычисляет общую стоимость покупки и печататет её на экране              *");
+            Console.WriteLine("* вычисляет значение по формуле и печатает его на экране                  *");
+            Console.WriteLine("* |(1 + x)^2 - y|" + String.Concat(Enumerable.Repeat(" ", 57)) + "*");
+            Console.WriteLine("* -------------" + String.Concat(Enumerable.Repeat(" ", 59)) + "*");
+            Console.WriteLine("*    (x + y)" + String.Concat(Enumerable.Repeat(" ", 62)) + "*");
             Console.WriteLine("*" + String.Concat(Enumerable.Repeat(" ", 73)) + "*");
             Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:" + String.Concat(Enumerable.Repeat(" ", 56)) + "*");
             Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
 
-            Console.WriteLine("Введите цену тетради (рубли):                                             *");
-            priceNote = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("* Введите число x" + String.Concat(Enumerable.Repeat(" ", 57)) + "*");
+            x = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Введите количевство тетрадей:                                             *");
-            amountNote = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Введите цену карандаша (рубли):                                           *");
-            pricePencil = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Введите количевство карандашей:                                           *");
-            amountPencil = Convert.ToInt32(Console.ReadLine());
-
+            Console.WriteLine("* Введите число y" + String.Concat(Enumerable.Repeat(" ", 57)) + "*");
+            y = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
             Console.WriteLine("Результат" + String.Concat(Enumerable.Repeat(" ", 65)) + "*");
             Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
 
-            Console.WriteLine($"Общая стоимость покупки состовляет {ds.PurchaseAmount(priceNote, amountNote, pricePencil, amountPencil)} рублей");
+
+            Console.WriteLine("* Значение выражения равно:");
+            Console.WriteLine($"* |(1 + {x})^2 - {y}|");
+            Console.WriteLine($"* --------------     == {ds.Calculate(x, y)}");
+            Console.WriteLine($"*  ({x} + {y})");
+            
+            
             
             Console.ReadKey();
+
         }
     }
 }
