08fade3f05ecd41e563989b1dbad438fdfaec5c5
Tyuiu.KosovAS@outlook.com <andrey.kosov.05@inbox.ru>
2023-09-26 19:35:36 +0500

    Task5

---
+++ Tyuiu.KosovAS.Sprint1.Task5.V2.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq.Expressions;
+using Tyuiu.KosovAS.Sprint1.Task5.V2.Lib;
+
+namespace Tyuiu.KosovAS.Sprint1.Task5.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double t = 100;
+            var res = 37;
+            Assert.AreEqual(res, ds.FahrenheitToСelsius(t));
+        }
+    }
+}
--- Tyuiu.KosovAS.Sprint1.Task5.V2/Program.cs
+++ Tyuiu.KosovAS.Sprint1.Task5.V2/Program.cs
@@ -4,54 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using Tyuiu.KosovAS.Sprint1.Task4.V21.Lib;
+using Tyuiu.KosovAS.Sprint1.Task5.V2.Lib;
 
-namespace Tyuiu.KosovAS.Sprint1.Task4.V21
+namespace Tyuiu.KosovAS.Sprint1.Task5.V2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             DataService ds = new DataService();
-            double x; double y;
+            double temp;
 
 
             Console.Title = "Спринт #1 | Выполнил: Косов А. С. | АСОиУБ-23-3";
             Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
             Console.WriteLine("* Спринт #1" + String.Concat(Enumerable.Repeat(" ", 63)) + "*");
-            Console.WriteLine("* Тема: Class Math" + String.Concat(Enumerable.Repeat(" ", 56)) + "*");
-            Console.WriteLine("* Задание #4" + String.Concat(Enumerable.Repeat(" ", 62)) + "*");
-            Console.WriteLine("* Вариант #21" + String.Concat(Enumerable.Repeat(" ", 61)) + "*");
+            Console.WriteLine("* Тема: Преобразование типов и класс Convert" + String.Concat(Enumerable.Repeat(" ", 30)) + "*");
+            Console.WriteLine("* Задание #5" + String.Concat(Enumerable.Repeat(" ", 62)) + "*");
+            Console.WriteLine("* Вариант #2" + String.Concat(Enumerable.Repeat(" ", 62)) + "*");
             Console.WriteLine("* Выполнил: Косов Андрей Сергеевич | АСОиУБ-23-3" + String.Concat(Enumerable.Repeat(" ", 26)) + "*");
             Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
             Console.WriteLine("* УСЛОВИЕ:" + String.Concat(Enumerable.Repeat(" ", 64)) + "*");
             Console.WriteLine("* Написать програму, которая запрашивает у пользователя исходные данные,  *");
-            Console.WriteLine("* вычисляет значение по формуле и печатает его на экране                  *");
-            Console.WriteLine("* |(1 + x)^2 - y|" + String.Concat(Enumerable.Repeat(" ", 57)) + "*");
-            Console.WriteLine("* -------------" + String.Concat(Enumerable.Repeat(" ", 59)) + "*");
-            Console.WriteLine("*    (x + y)" + String.Concat(Enumerable.Repeat(" ", 62)) + "*");
+            Console.WriteLine("* переводит температуру из Форенгейтов в градусы Цельсия.                 *");
             Console.WriteLine("*" + String.Concat(Enumerable.Repeat(" ", 73)) + "*");
             Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:" + String.Concat(Enumerable.Repeat(" ", 56)) + "*");
             Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
 
-            Console.WriteLine("* Введите число x" + String.Concat(Enumerable.Repeat(" ", 57)) + "*");
-            x = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("* Введите число y" + String.Concat(Enumerable.Repeat(" ", 57)) + "*");
-            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите температуру (F):");
+            temp = Convert.ToDouble(Console.ReadLine());
+
 
             Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
             Console.WriteLine("Результат" + String.Concat(Enumerable.Repeat(" ", 65)) + "*");
             Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
 
-
-            Console.WriteLine("* Значение выражения равно:");
-            
-            Console.WriteLine($"* |(1 + {x})^2 - {y}| / ({x} + {y}) == {ds.Calculate(x, y)}");
-            
-            
+            Console.WriteLine($"Температура равна {ds.FahrenheitToСelsius(temp)} градусов по Цельсия.");
             Console.ReadKey();
+
+
+
 
         }
     }
